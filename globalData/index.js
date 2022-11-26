d867a8136028d29d745f3f5fd12feb3d041edf11
Ku mohammad yusri Ku ibrahim <kmyusri@MacBook-Air-2.local>
2022-11-26 09:51:01 +0800

    init

---
+++ globalData/index.js
@@ -0,0 +1,11 @@
+module.exports.MAIL_TEMPLATE_TYPE={
+    Welcome: "Welcome",
+    KickStudent: "Kick Student",
+    DeleteCourse: "Delete Course",
+    WithdrawCourse: "Withdraw Course",
+    CreateAttendance: "Create Attendance",
+
+}
+
+module.exports.OfficialURL="http://localhost:3000";
+//module.exports.OfficialURL="https://attendlytical.netlify.app";
